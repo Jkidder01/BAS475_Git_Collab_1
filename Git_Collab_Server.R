26290bcc8ec48d044d2027408b2b14e4bf9a351f
alexpayne5 <apayne29@vols.utk.edu>
2022-02-08 14:12:04 -0500

    Contribution_2_autoplot

--- Git_Collab_Server.R
+++ Git_Collab_Server.R
@@ -1,6 +1,15 @@
-library(shiny)
+library('shiny')
 
 
 server <- function(input, output) {
 
+  output$t.plot <- renderPlot({
+
+  region_plot <- tourism[tourism$Region == "Melbourne", ]
+
+  autoplot(region_plot)
+
+  })
 }
+
+shinyApp(ui, server)
